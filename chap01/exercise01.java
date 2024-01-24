a90b6e6ef7650bd1c9bdee3898cae2e7627d1f38
vJYC <zuimashi64@gmail.com>
2024-01-24 16:31:51 +0900

    확인문제 5번

---
+++ chap01/exercise01.java
@@ -0,0 +1,12 @@
+package chap01; //확인문제 05
+
+public class exercise01 {
+    public static void main(String[] args) {
+        for(int x = 0; x <=10; x++) {
+            for(int y = 0; y <=10; y++) {
+                if((4 * x) + (5 * y) == 60)
+                    System.out.println("(" + x + ", " + y + ")");
+            }
+        }
+    }
+}
